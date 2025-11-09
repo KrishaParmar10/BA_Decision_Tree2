b66b78dcd423026261722e0dab91a298437cf204
Krisha Parmar <parmarkrisha10@gmail.com>
2025-11-09 13:36:59 +0530

    Rename model filename to decision_tree_model.pkl

--- app (5) (1).py
+++ app (5) (1).py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import joblib
 
-filename = 'filename.pkl'
+filename = 'decision_tree_model.pkl'
 loaded_model = joblib.load(open(filename, 'rb'))
 
 # Define the correct column names
